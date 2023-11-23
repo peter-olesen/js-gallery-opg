06856c62493151ee674fbfc57a45ac66613a48b0
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2023-11-23 13:49:39 +0100

    first draft

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,53 @@
-// Din cool code here
+const myImages = ['elephant.jpg', 'great-white.jpg', 'koala.jpg', 'smithi.jpg', 'tiger.jpg'];
+
+/* const myGalleryElement = document.getElementById('gallery');
+
+myImages.forEach((image) => {
+    let myImage = document.createElement('img');
+
+    myImage.src = 'assets/img/' + image;
+
+    myGalleryElement.appendChild(myImage);
+});  */
+
+buildGallery();
+
+function buildGallery(){
+    const myGalleryElement = document.getElementById('gallery');
+    myImages.forEach(image => {
+        buildCard('assets/img/' + image, myGalleryElement)
+    });
+}
+
+function buildCard(imageUrl, myGalleryElement){
+    const myArticle = document.createElement('article');
+    const myImage = document.createElement('img');
+
+    myImage.src = imageUrl;
+
+    myImage.addEventListener('click', (event) =>{
+        myGalleryElement.innerHTML = '';
+        
+        buildBigImage(imageUrl, myGalleryElement);
+    });
+
+    myArticle.appendChild(myImage);
+
+    myGalleryElement.appendChild(myArticle);
+}
+
+function buildBigImage(imageUrl, myGalleryElement){
+    const myArticle = document.createElement('article');
+    const myImage = document.createElement('img');
+
+    myImage.src = imageUrl;
+
+    myImage.addEventListener('click', (event) =>{
+        myGalleryElement.innerHTML = '';
+        buildGallery();
+    });
+
+    myArticle.appendChild(myImage);
+
+    myGalleryElement.appendChild(myArticle);
+}
